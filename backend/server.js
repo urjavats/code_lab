587efb484d0627b7ef89b0a533a24984269e2ca7
UtkarshVikramSingh333 <utkarshvikramsingh333@gmail.com>
2025-01-06 17:30:08 -0500

    vercel

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
-
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 const cors = require('cors');
 const server = require("http").createServer(app);
 // Create Socket.IO instance
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: FRONTEND_URL,
       methods: ["GET", "POST"],
       credentials: true,
       transports: ['websocket', 'polling']
@@ -55,7 +55,12 @@ const io = new Server(server, {
 const bodyParser=require('express').json;
 app.use(bodyParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: FRONTEND_URL, // Allow frontend to access backend
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
 
 const userRouter=require('./api/User');
 const roomRouter=require('./api/Room');
