0d8045127f49e4c686fe6b5a9de902c8e1076168
Urja Vats <vatsurja@gmail.com>
2025-01-06 23:51:26 -0500

    socket changes

--- backend/api/socket.js
+++ backend/api/socket.js
@@ -7,15 +7,16 @@ const { Server } = require('socket.io');
 const app = express();
 // const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 const cors = require('cors');
-const server = require("http").createServer(app);
 
 const allowedOrigins = [
   "http://localhost:3000", 
   "https://code-lab-pu8s.vercel.app"
 ];
 
+
 // Create Socket.IO instance
-const io = new Server(server, {
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
     cors: {
       origin: allowedOrigins,
       methods: ["GET", "POST"],
@@ -81,19 +82,17 @@ app.use((req, res, next) => {
 
 
 
-const userRouter=require('./api/User');
-const roomRouter=require('./api/Room');
+const userRouter=require('/User');
+const roomRouter=require('/Room');
 app.use('/user',userRouter);
 app.use('/room',roomRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
-// Allow CORS so that backend and frontend could be put on different servers
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+module.exports = (req, res) => {
+  httpServer.emit('request', req, res); // Forward the request to the HTTP server
+};
 
 
 
