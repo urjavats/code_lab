0a9b583c7f569a2bfa65a7c8ad27f5c94f1c0013
Urja Vats <vatsurja@gmail.com>
2025-01-06 21:42:23 -0500

    changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -43,7 +43,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch(`$API_BASE_URL/user/signin`, {
+      const response = await fetch(`${API_BASE_URL}/user/signin`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
